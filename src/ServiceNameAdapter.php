a02624ff45e0685c697d6266fa5d52ec9bb1a727
Fish <chao.yen.po@gmail.com>
2020-02-29 00:01:45 +0800

    Add base method

--- src/ServiceNameAdapter.php
+++ src/ServiceNameAdapter.php
@@ -4,5 +4,83 @@ namespace FishpondServices\:service_namespace;
 
 class :service_nameAdapter
 {
-    //
+    /**
+     * @inheritdoc
+     */
+    public function getGameList(TypeInterface $type = null)
+    {
+        //
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function prepareCreatePlayer(PlayerInterface $player, Config $config)
+    {
+        //
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function createPlayer(PlayerInterface $player, Config $config)
+    {
+        //
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getLoginUrl(PlayerInterface $player, GameInterface $game, Config $config)
+    {
+        //
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function logout(PlayerInterface $player, GameInterface $game, Config $config)
+    {
+        //
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getBalance(PlayerInterface $player, Config $config)
+    {
+        //
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function prepareTransfer(TransactionInterface $transaction, Config $config)
+    {
+        //
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function transfer(TransactionInterface $transaction, Config $config)
+    {
+        //
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getTransferRecord(TransactionInterface $transaction, Config $config)
+    {
+        //
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getRecordDetailUrl(RecordInterface $record, GameInterface $game, Config $config)
+    {
+        //
+    }
 }
