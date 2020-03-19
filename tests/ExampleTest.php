88be17b611c66ea04b896e341577b11d13ee48da
wesley.peng <ep455103@gmail.com>
2020-03-19 11:17:48 +0800

    first Version

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,52 +6,142 @@ use PHPUnit\Framework\TestCase;
 
 use FishpondServices\:service_namespace\:service_nameClient as Client;
 use FishpondServices\:service_namespace\:service_nameAdapter as Adapter;
+use FishpondServices\:service_namespace\:service_nameBetRecordMethod as BetRecordMethod;
 class ExampleTest extends TestCase
 {
+    private static $client;
+    private static $adapter;
+    private static $player;
+    private static $config;
+    private static $game;
+    private static $recordType;
+    private static $gameType;
+    private static $betRecordMethod;
+    private static $payUpTransaction;
+    private static $payDownTransaction;
+    private static $record;
+
     private $credentials = [
 
     ];
 
-    /** @test */
-    public function true_is_true()
+    private static $configData = [
+
+    ];
+
+    protected function featureForTest()
     {
-        $this->assertTrue(true);
+        return [
+            'create_member',
+            'login',
+            'logout',
+            'balance',
+            'transfer',
+            'check_transfer',
+            'report',
+            'gameList',
+            'urlDetail',
+        ];
     }
 
-    /** @test */
-    public function authTest()
+    public static function setupBefore()
     {
-        $this->markTestSkipped();
+        self::$adapter = new Adapter(new Client(['credentials' =>self::$credentials]));
 
-        $client = new Client([
-            'credentials' => $this->credentials,
-        ]);
+        self::$config = new Config(self::$configData);
 
-        $adapter = new Adapter($client);
+        self::$player = self::$adapter->prepareCreatePlayer(new Player(), self::$config)['player'];
+
+        self::$game = new Game(X,X,X,X);
+
+        self::$payUpTransaction = new Transaction(:transfor amount);
+
+        self::$payDownTransaction = new Transaction(:transfor amount);
+
+        self::$recordType = new RecordType(1);
+
+        self::$gameType = new GameType(TypeInterface::XXX);
+
+        self::$betRecordMethod = new BetRecordMethod(self::$config);
+
+        self::$record = new Record();
     }
 
-    /** @test */
-    public function transferTest()
-    {
-        $this->markTestSkipped();
+     /** @test */
+     public function createMember()
+     {
+         if (!in_array("create_member", $this->featureForTest())) {
+             $this->markTestSkipped();
+         }
 
-        $client = new Client([
-            'credentials' => $this->credentials,
-        ]);
+         $this->assertArrayHasKey('player', $response);
+     }
 
-        $adapter = new Adapter($client);
-    }
+     /** @test */
+     public function login()
+     {
+         if (!in_array("login", $this->featureForTest())) {
+             $this->markTestSkipped();
+         }
 
-    /** @test */
-    public function recordTest()
-    {
-        $this->markTestSkipped();
+         $this->assertArrayHasKey('loginUrl', $response);
+     }
 
-        $client = new Client([
-            'credentials' => $this->credentials,
-        ]);
+     /** @test */
+     public function logout()
+     {
+         if (!in_array("logout", $this->featureForTest())) {
+             $this->markTestSkipped();
+         }
 
-        $adapter = new Adapter($client);
-    }
+         $this->assertTrue($response);
+     }
 
+     /** @test */
+     public function balance()
+     {
+         if (!in_array("balance", $this->featureForTest())) {
+             $this->markTestSkipped();
+         }
+
+         $this->assertArrayHasKey('balance', $response);
+     }
+
+     /** @test */
+     public function transfer()
+     {
+         if (!in_array("transfer", $this->featureForTest())) {
+             $this->markTestSkipped();
+         }
+
+         $this->assertArrayHasKey('transaction', $response);
+     }
+
+     /** @test */
+     public function checkTransfer()
+     {
+         if (!in_array("check_transfer", $this->featureForTest())) {
+             $this->markTestSkipped();
+         }
+
+         $this->assertArrayHasKey('transaction', $response);
+     }
+     /** @test */
+     public function gameList()
+     {
+         if (!in_array("gameList", $this->featureForTest())) {
+             $this->markTestSkipped();
+         }
+
+         $this->assertIsArray($response);
+     }
+
+     /** @test */
+     public function fetchRecords()
+     {
+         if (!in_array("report", $this->featureForTest())) {
+             $this->markTestSkipped();
+
+         $this->assertIsArray($response);
+     }
 }
